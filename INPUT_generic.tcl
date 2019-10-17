18e41330c4f0f195878b96b53c47318055fa08e1
sadilak85 <56599783+sadilak85@users.noreply.github.com>
2019-10-17 15:50:46 +0200

    Add files via upload

--- INPUT_generic.tcl
+++ INPUT_generic.tcl
@@ -6,24 +6,29 @@ set NStory n;			# number of stories above ground level
 set NBay n;				# number of bays in X direction
 #NBAYZ
 set NBayZ n;			# number of bays in Z direction
-#NODE
+#FREENODE
+set FreeNodeID n;					#n ID: free node  to output results
+#GROUNDNODES
 node NodeID X Y Z	.... corners on Ground Level including Bays
 node NodeID X Y Z
 node NodeID X Y Z
 node NodeID X Y Z
+#STOREYNODES_first
 node NodeID X Y Z	.... corners on 1st Floor Ground including Bays
 node NodeID X Y Z
 node NodeID X Y Z
 node NodeID X Y Z
+#STOREYNODES_second
 node NodeID X Y Z	.... corners on 2nd Floor Ground including Bays
 node NodeID X Y Z
 node NodeID X Y Z
 node NodeID X Y Z
+#STOREYNODES_third
 node NodeID X Y Z  .... corners on the Roof plane
 node NodeID X Y Z
 node NodeID X Y Z
 node NodeID X Y Z
-#MASTERNODE     .... Geometrically center node on each Storey
+#MASTERNODES     .... Geometrically center node on each Storey
 node MasterNodeID X Y Z	.... Ground Level 
 node MasterNodeID X Y Z	.... 1st Floor
 node MasterNodeID X Y Z	.... 2nd Floor
@@ -40,13 +45,13 @@ element nonlinearBeamColumn ElementID NodeID NodeID 5 2 2 .... 2nd Floor
 element nonlinearBeamColumn ElementID NodeID NodeID 5 2 2
 element nonlinearBeamColumn ElementID NodeID NodeID 5 2 2 .... Roof Plane
 element nonlinearBeamColumn ElementID NodeID NodeID 5 2 2
-#BEAMLENGTH
-  ElementID LENGTH
-  ElementID LENGTH
-  ElementID LENGTH
-  ElementID LENGTH
-  ElementID LENGTH
-  ElementID LENGTH
+#BEAMLENGTH    .... 288 Zoll is normally used
+set Beamlength_EltID LENGTH
+set Beamlength_EltID LENGTH
+set Beamlength_EltID LENGTH
+set Beamlength_EltID LENGTH	
+set Beamlength_EltID LENGTH
+set Beamlength_EltID LENGTH
 #GIRDER  -- parallel to z axis
 element nonlinearBeamColumn ElementID NodeID NodeID 5 3 3  .... 1st Floor
 element nonlinearBeamColumn ElementID NodeID NodeID 5 3 3
@@ -54,13 +59,13 @@ element nonlinearBeamColumn ElementID NodeID NodeID 5 3 3  .... 2nd Floor
 element nonlinearBeamColumn ElementID NodeID NodeID 5 3 3
 element nonlinearBeamColumn ElementID NodeID NodeID 5 3 3 .... Roof Plane
 element nonlinearBeamColumn ElementID NodeID NodeID 5 3 3
-*GIRDERLENGTH
-  ElementID LENGTH
-  ElementID LENGTH
-  ElementID LENGTH
-  ElementID LENGTH
-  ElementID LENGTH
-  ElementID LENGTH
+#GIRDERLENGTH
+set Beamlength_EltID LENGTH
+set Beamlength_EltID LENGTH
+set Beamlength_EltID LENGTH
+set Beamlength_EltID LENGTH	
+set Beamlength_EltID LENGTH
+set Beamlength_EltID LENGTH
 # COLUMN   -- parallel to y axis
 # 5 is number of Gauss integration points for nonlinear curvature distribution, 1's are unique Tags
 element nonlinearBeamColumn ElementID NodeID NodeID 5 1 1 .... 1st Floor 
@@ -69,7 +74,7 @@ element nonlinearBeamColumn ElementID NodeID NodeID 5 1 1 .... 2nd Floor
 element nonlinearBeamColumn ElementID NodeID NodeID 5 1 1
 element nonlinearBeamColumn ElementID NodeID NodeID 5 1 1 .... Roof Plane
 element nonlinearBeamColumn ElementID NodeID NodeID 5 1 1
-#COLUMNLENGTH
-  FixColumnLength
-#
+#COLUMNLENGTH    ...  168 Zoll is normally used
+set Columnlength_fix LENGTH    
+#END
 
